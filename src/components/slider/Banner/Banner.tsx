68d127af247957364af3dbff6ee1ca2240e56671
sunny7657 <ksvetlana466@gmail.com>
2024-06-13 19:20:51 +0200

    add: banner background styles

--- src/components/slider/Banner/Banner.tsx
+++ src/components/slider/Banner/Banner.tsx
@@ -8,33 +8,27 @@ import BannerBackground from "../BannerBackground/BannerBackground";
 const Banner = ({ item }: BannerProps) => {
   const { label, title, condition, price, buttonName, backgroundImages } = item;
 
-  // const bannerStyle = {
-  //   backgroundImage: `url(${backgroundImages.mobile1x})`,
-  // }; style={bannerStyle}
-
   return (
-    <div className={styles.bannerContainer}>
-      <BannerBackground backgroundImages={backgroundImages}>
-        <div className={styles.bannerLabelContainer}>
-          <CommonIcon
-            id="icon-star"
-            width="40"
-            height="26"
-            className={styles.bannerIcon}
-          />
-          <span className={styles.bannerLabel}>{label}</span>
-        </div>
-        <h2 className={styles.bannerTitle}>{title}</h2>
-        <p className={styles.bannerAdditionalInfo}>{condition || "\u00A0"}</p>
-        <p className={styles.bannerPrice}>{price || "\u00A0"}</p>
-        <CommonButton
-          type="button"
-          title={buttonName}
-          color="yellow"
-          className={styles.bannerButton}
+    <BannerBackground backgroundImages={backgroundImages}>
+      <div className={styles.bannerLabelContainer}>
+        <CommonIcon
+          id="icon-star"
+          width="40"
+          height="26"
+          className={styles.bannerIcon}
         />
-      </BannerBackground>
-    </div>
+        <span className={styles.bannerLabel}>{label}</span>
+      </div>
+      <h2 className={styles.bannerTitle}>{title}</h2>
+      <p className={styles.bannerAdditionalInfo}>{condition || "\u00A0"}</p>
+      <p className={styles.bannerPrice}>{price || "\u00A0"}</p>
+      <CommonButton
+        type="button"
+        title={buttonName}
+        color="yellow"
+        className={styles.bannerButton}
+      />
+    </BannerBackground>
   );
 };
 
--- src/components/slider/SliderComponent/SliderComponent.tsx
+++ src/components/slider/SliderComponent/SliderComponent.tsx
@@ -1,18 +1,15 @@
 "use client";
-import styles from "./SliderComponent.module.scss";
 
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { useRef } from "react";
 import Banner from "../Banner/Banner";
 import { bannerItems } from "@/mock-data/bannerItems";
 import { nanoid } from "nanoid";
 
 const SliderComponent = () => {
-  // const slider = useRef(null); ref={slider}
   const settings = {
     className: "center",
     dots: true,
@@ -38,9 +35,7 @@ const SliderComponent = () => {
   return (
     <Slider {...settings}>
       {bannerItems.map((item) => (
-        <div className={styles.bannerContainer} key={nanoid()}>
-          <Banner item={item} />
-        </div>
+        <Banner item={item} key={nanoid()} />
       ))}
     </Slider>
   );
