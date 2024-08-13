890d348dbaefdb04b9244d5061187f79bd696888
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-08-13 20:29:54 +0300

    create and replace CommonInput to ui

--- src/components/auth/Login/Login.styled.ts
+++ src/components/auth/Login/Login.styled.ts
@@ -9,44 +9,11 @@ export const Form = styled.form`
   gap: 24px;
 `;
 
-export const InputTitle = styled.label`
-  color: var(--label);
-  font-family: Inter;
-  font-size: 16px;
-  font-weight: 400;
-  &::after {
-    content: " *";
-    color: red;
-  }
-`;
-
-export const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 4px;
-`;
-
-export const Input = styled.input`
-  width: 420px;
-  height: 52px;
-  padding: 14px 12px;
-  border-radius: 10px;
-  border: 1px solid var(--input-text);
-  background: var(--background);
-  cursor: pointer;
-  &:focus {
-    border: 2px solid var(--accent);
-    outline: none;
-  }
-`;
-
 export const TogglePasswordButton = styled.button`
   margin-left: 8px;
 
   cursor: pointer;
 `;
-
 
 export const SubmitButton = styled.button`
   padding: 20px 185px;
---
+++ src/components/ui/CommonInput/CommonInput.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode, ChangeEvent } from "react";
+import { Input, InputTitle, InputContainer } from "./CommonInput.styled";
+
+type CommonInputProps = {
+  text: string;
+  typeTitle: string;
+  typeInput: boolean | string;
+  value: string;
+  id?: string;
+  setValue: (e: ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  placeholder: string;
+  required?: boolean;
+  children?: ReactNode;
+  errorsMessage?: ReactNode;
+};
+
+const CommonInput: FC<CommonInputProps> = ({
+  text,
+  typeTitle,
+  typeInput,
+  value,
+  setValue,
+  className,
+  placeholder,
+  required,
+  children,
+  errorsMessage,
+}) => {
+  return (
+    <InputContainer>
+      <InputTitle htmlFor={typeTitle}>{text}</InputTitle>
+      <div style={{ position: "relative" }}>
+        <Input
+          type={typeInput ? "text" : "password"}
+          id="password"
+          value={value}
+          onChange={setValue}
+          className={className}
+          required={required}
+          placeholder={placeholder}
+        />
+        {children}
+      </div>
+      {errorsMessage}
+    </InputContainer>
+  );
+};
+
+export default CommonInput;
