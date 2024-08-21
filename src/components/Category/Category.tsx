2643da48fc9402d372d006e7aab6b572c2761a59
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-08-21 15:40:28 +0300

    after change

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,95 +1,167 @@
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+// import { fetchCategoryData } from "@/api/fetchCategoryData";
+// import CategoryItem from "@/types/categoryItemType";
+// import {
+//  Error,
+//  Loading,
+//  SectionContainer,
+//  TitleContainer,
+//  Title,
+//  ButtonsContainer,
+//  BuyButton,
+//  SellButton,
+//  List,
+//  ListItem,
+//  FoldImg,
+//  ListItemWrapper,
+//  ListItemImage,
+//  ListItemText
+// } from './CategoryComponent'
+
+// const Category: React.FC = () => {
+//  const [data, setData] = useState<CategoryItem[] | null>(null);
+//  const [loading, setLoading] = useState<boolean>(true);
+//  const [error, setError] = useState<string | null>(null);
+
+//  useEffect(() => {
+//   const fetchData = async () => {
+//    try {
+//     const result = await fetchCategoryData();
+//     setData(result);
+//    } catch (error: any) {
+//     console.error("Error fetching data:", error);
+//     setError(error.message);
+//    } finally {
+//     setLoading(false);
+//    }
+//   };
+
+//   fetchData();
+//  }, []);
+
+//  if (loading) {
+//   return <Loading>Loading...</Loading>;
+//  }
+
+//  if (error) {
+//   return <Error>Error: {error}</Error>;
+//  }
+
+//  return (
+//   <section>
+//    <SectionContainer>
+//     <TitleContainer>
+//      <Title />
+//      <ButtonsContainer>
+//       <BuyButton>Куплю</BuyButton>
+//       <SellButton>Продам</SellButton>
+//      </ButtonsContainer>
+//     </TitleContainer>
+//     {
+//      data && data.length > 0 ? (
+//       <List>
+//        {data.map(({ id, nameUkr, photoUrl }) => (
+//         <ListItem key={id}>
+//          <FoldImg />
+//          <ListItemWrapper>
+//           <ListItemImage
+//            src={photoUrl}
+//            alt={nameUkr}
+//            width={98}
+//            height={98}
+//           />
+//           <ListItemText>{nameUkr}</ListItemText>
+//          </ListItemWrapper>
+//         </ListItem>
+//        ))}
+//       </List>
+//      ) : (
+//       <p>No data available</p>
+//      )
+//     }
+//    </SectionContainer>
+//   </section >
+//  );
+// };
+
+// export default Category;
+
 "use client";
 
-import React, { useState, useEffect } from "react";
-// import Image from "next/image";
+import { useState, useEffect } from "react";
 import { fetchCategoryData } from "@/api/fetchCategoryData";
+import Image from "next/image";
+import Fold from "@/assets/Svg/fold.svg";
 import CategoryItem from "@/types/categoryItemType";
-// import style from "./Category.module.scss";
-// import styles from './category.module.scss'
-import {
- Error,
- Loading,
- SectionContainer,
- TitleContainer,
- Title,
- ButtonsContainer,
- BuyButton,
- SellButton,
- List,
- ListItem,
- FoldImg,
- ListItemWrapper,
- ListItemImage,
- ListItemText
-} from './CategoryComponent'
-import { log } from "console";
-
-
+import styles from "./Category.module.scss";
 
 const Category: React.FC = () => {
- const [data, setData] = useState<CategoryItem[] | null>(null);
- const [loading, setLoading] = useState<boolean>(true);
- const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<CategoryItem[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
- useEffect(() => {
-  const fetchData = async () => {
-   try {
-    const result = await fetchCategoryData();
-    setData(result);
-   } catch (error: any) {
-    console.error("Error fetching data:", error);
-    setError(error.message);
-   } finally {
-    setLoading(false);
-   }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetchCategoryData();
+        setData(result);
+      } catch (error: any) {
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  fetchData();
- }, []);
+    fetchData();
+  }, []);
 
- if (loading) {
-  return <Loading>Loading...</Loading>;
- }
+  if (loading) {
+    return <div className={styles.loading}>Loading...</div>;
+  }
 
- if (error) {
-  return <Error>Error: {error}</Error>;
- }
+  if (error) {
+    return <div className={styles.error}>Error: {error}</div>;
+  }
 
- return (
-  <section>
-   <SectionContainer>
-    <TitleContainer>
-     <Title />
-     <ButtonsContainer>
-      <BuyButton>Куплю</BuyButton>
-      <SellButton>Продам</SellButton>
-     </ButtonsContainer>
-    </TitleContainer>
-    {
-     data && data.length > 0 ? (
-      <List>
-       {data.map(({ id, nameUkr, photoUrl }) => (
-        <ListItem key={id}>
-         <FoldImg />
-         <ListItemWrapper>
-          <ListItemImage
-           src={photoUrl}
-           alt={nameUkr}
-           width={98}
-           height={98}
-          />
-          <ListItemText>{nameUkr}</ListItemText>
-         </ListItemWrapper>
-        </ListItem>
-       ))}
-      </List>
-     ) : (
-      <p>No data available</p>
-     )
-    }
-   </SectionContainer>
-  </section >
- );
+  return (
+    <section>
+      <div className={styles.sectionContainer}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}></h2>
+          <div className={styles.buttonsContainer}>
+            <div className={styles.buyButton}>Куплю</div>
+            <div className={styles.sellButton}>Продам</div>
+          </div>
+        </div>
+
+        {data && data.length > 0 ? (
+          <ul className={styles.list}>
+            {data.map(({ id, nameUkr, photoUrl }) => (
+              <li className={styles.listItem} key={id}>
+                <Image className={styles.foldImg} src={Fold} alt="Fold" />
+                <div className={styles.listItemWrapper}>
+                  <Image
+                    className={styles.listItemImage}
+                    src={photoUrl}
+                    alt={nameUkr}
+                    width={98}
+                    height={98}
+                    style={{ objectFit: "cover" }}
+                  />
+                  <p className={styles.listItemText}>{nameUkr}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Category;
--- src/components/Category/CategoryComponent.jsx
+++ src/components/Category/CategoryComponent.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
-import styles from './Category.module.scss'
-import Fold from "@/assets/Svg/fold.svg";
-const Error = ({ children }) => <div className={styles.error}>{children}</div>;
-const Loading = ({ children }) => <div className={styles.loading}>{children}</div>
-const SectionContainer = ({ children }) => <div className={styles.sectionContainer}>{children}</div>
-const TitleContainer = ({ children }) => <div className={styles.titleContainer}>{children}</div>
-const Title = () => <h2 className={styles.title}></h2>
-const ButtonsContainer = ({ children }) => <div className={styles.buttonsContainer}>{children}</div>
-const BuyButton = ({ children }) => <div className={styles.buyButton}>{children}</div>
-const SellButton = ({ children }) => <div className={styles.sellButton}>{children}</div>
-const List = ({ children }) => <ul className={styles.list}>{children}</ul>
-const ListItem = ({ children }) => <li className={styles.listItem}>{children}</li>
-const FoldImg = () => <img className={styles.foldImg} src={Fold} alt="Fold" />
-const ListItemWrapper = ({ children }) => <div className={styles.listItemWrapper}>{children}</div>
-const ListItemImage = ({ src, alt, width, height }) => <img className={styles.listItemImage} src={src} alt={alt} width={width} height={height} style={{ objectFit: 'cover' }} />
-const ListItemText = ({ children }) => <p className={styles.listItemText}>{children}</p>
-export { Error, Loading, SectionContainer, TitleContainer, Title, ButtonsContainer, BuyButton, SellButton, List, ListItem, FoldImg, ListItemWrapper, ListItemImage, ListItemText, }
+// import React from "react";
+// import styles from "./Category.module.scss";
+// import Fold from "@/assets/Svg/fold.svg";
+
+// const Error = ({ children }) => <div className={styles.error}>{children}</div>;
+// const Loading = ({ children }) => (
+//   <div className={styles.loading}>{children}</div>
+// );
+// const SectionContainer = ({ children }) => (
+//   <div className={styles.sectionContainer}>{children}</div>
+// );
+// const TitleContainer = ({ children }) => (
+//   <div className={styles.titleContainer}>{children}</div>
+// );
+// const Title = () => <h2 className={styles.title}></h2>;
+// const ButtonsContainer = ({ children }) => (
+//   <div className={styles.buttonsContainer}>{children}</div>
+// );
+// const BuyButton = ({ children }) => (
+//   <div className={styles.buyButton}>{children}</div>
+// );
+// const SellButton = ({ children }) => (
+//   <div className={styles.sellButton}>{children}</div>
+// );
+// const List = ({ children }) => <ul className={styles.list}>{children}</ul>;
+// const ListItem = ({ children }) => (
+//   <li className={styles.listItem}>{children}</li>
+// );
+// const FoldImg = () => <img className={styles.foldImg} src={Fold} alt="Fold" />;
+// const ListItemWrapper = ({ children }) => (
+//   <div className={styles.listItemWrapper}>{children}</div>
+// );
+// const ListItemImage = ({ src, alt, width, height }) => (
+//   <img
+//     className={styles.listItemImage}
+//     src={src}
+//     alt={alt}
+//     width={width}
+//     height={height}
+//     style={{ objectFit: "cover" }}
+//   />
+// );
+// const ListItemText = ({ children }) => (
+//   <p className={styles.listItemText}>{children}</p>
+// );
+// export {
+//   Error,
+//   Loading,
+//   SectionContainer,
+//   TitleContainer,
+//   Title,
+//   ButtonsContainer,
+//   BuyButton,
+//   SellButton,
+//   List,
+//   ListItem,
+//   FoldImg,
+//   ListItemWrapper,
+//   ListItemImage,
+//   ListItemText,
+// };
