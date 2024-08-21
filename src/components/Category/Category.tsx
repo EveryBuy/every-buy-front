ac13fd830ae15599aa667f7340b8b10da19a6cc4
Rostyslav <rostyslavborovyk1201@gmail.com>
2024-08-21 14:05:30 +0300

    change component category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -2,90 +2,94 @@
 
 import React, { useState, useEffect } from "react";
 // import Image from "next/image";
-import Fold from "@/assets/Svg/fold.svg";
 import { fetchCategoryData } from "@/api/fetchCategoryData";
 import CategoryItem from "@/types/categoryItemType";
-import {
-  SectionContainer,
-  TitleContainer,
-  Title,
-  ButtonsContainer,
-  BuyButton,
-  SellButton,
-  List,
-  ListItem,
-  ListItemWrapper,
-  ListItemText,
-  ListItemImage,
-  FoldImg,
-  Loading,
-  Error,
-} from "./Category.styled";
 // import style from "./Category.module.scss";
+// import styles from './category.module.scss'
+import {
+ Error,
+ Loading,
+ SectionContainer,
+ TitleContainer,
+ Title,
+ ButtonsContainer,
+ BuyButton,
+ SellButton,
+ List,
+ ListItem,
+ FoldImg,
+ ListItemWrapper,
+ ListItemImage,
+ ListItemText
+} from './CategoryComponent'
+import { log } from "console";
+
+
 
 const Category: React.FC = () => {
-  const [data, setData] = useState<CategoryItem[] | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+ const [data, setData] = useState<CategoryItem[] | null>(null);
+ const [loading, setLoading] = useState<boolean>(true);
+ const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetchCategoryData();
-        setData(result);
-      } catch (error: any) {
-        console.error("Error fetching data:", error);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+ useEffect(() => {
+  const fetchData = async () => {
+   try {
+    const result = await fetchCategoryData();
+    setData(result);
+   } catch (error: any) {
+    console.error("Error fetching data:", error);
+    setError(error.message);
+   } finally {
+    setLoading(false);
+   }
+  };
 
-    fetchData();
-  }, []);
+  fetchData();
+ }, []);
 
-  if (loading) {
-    return <Loading>Loading...</Loading>;
-  }
+ if (loading) {
+  return <Loading>Loading...</Loading>;
+ }
 
-  if (error) {
-    return <Error>Error: {error}</Error>;
-  }
+ if (error) {
+  return <Error>Error: {error}</Error>;
+ }
 
-  return (
-    <section>
-      <SectionContainer>
-        <TitleContainer>
-          <Title />
-          <ButtonsContainer>
-            <BuyButton>Куплю</BuyButton>
-            <SellButton>Продам</SellButton>
-          </ButtonsContainer>
-        </TitleContainer>
-        {data && data.length > 0 ? (
-          <List>
-            {data.map(({ id, nameUkr, photoUrl }) => (
-              <ListItem key={id}>
-                <FoldImg src={Fold} alt="Fold" />
-                <ListItemWrapper>
-                  <ListItemImage
-                    src={photoUrl}
-                    alt={nameUkr}
-                    width={98}
-                    height={98}
-                    style={{ objectFit: "cover" }}
-                  />
-                  <ListItemText>{nameUkr}</ListItemText>
-                </ListItemWrapper>
-              </ListItem>
-            ))}
-          </List>
-        ) : (
-          <p>No data available</p>
-        )}
-      </SectionContainer>
-    </section>
-  );
+ return (
+  <section>
+   <SectionContainer>
+    <TitleContainer>
+     <Title />
+     <ButtonsContainer>
+      <BuyButton>Куплю</BuyButton>
+      <SellButton>Продам</SellButton>
+     </ButtonsContainer>
+    </TitleContainer>
+    {
+     data && data.length > 0 ? (
+      <List>
+       {data.map(({ id, nameUkr, photoUrl }) => (
+        <ListItem key={id}>
+         <FoldImg />
+         <ListItemWrapper>
+          <ListItemImage
+           src={photoUrl}
+           alt={nameUkr}
+           width={98}
+           height={98}
+          />
+          <ListItemText>{nameUkr}</ListItemText>
+         </ListItemWrapper>
+        </ListItem>
+       ))}
+      </List>
+     ) : (
+      <p>No data available</p>
+     )
+    }
+   </SectionContainer>
+  </section >
+ );
 };
 
 export default Category;
