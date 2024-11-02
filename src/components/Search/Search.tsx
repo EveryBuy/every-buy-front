c0c487835d2cbf6f6f15d5a9fb108172cb25da11
AzVika <26838569+AzVika@users.noreply.github.com>
2024-11-02 19:07:35 +0200

    add search input functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,38 +1,55 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { CommonIcon, CommonButton } from "@/components";
 import styles from "./Search.module.scss";
+import { useRouter } from 'next/navigation';
 
-const Search: FC = () => {
-  return (
-    <div className={styles.searchContainer}>
-      <form className={styles.searchForm}>
-        <div className={styles.searchInputWrapper}>
-          <input className={styles.searchInput} placeholder="Що шукаєте?" />
-          <div className={styles.searchInputIconWrapper}>
-            <CommonIcon
-              id="icon-search"
-              width="28"
-              height="28"
-              className={styles.searchInputIcon}
-            />
-          </div>
-        </div>
-        <CommonButton
-          type="submit"
-          title=""
-          color="white"
-          className={styles.searchButton}
-        >
-          <CommonIcon
-            id="icon-search"
-            width="25"
-            height="25"
-            className={styles.searchButtonIcon}
-          />
-        </CommonButton>
-      </form>
-    </div>
-  );
+const Search: FC = (props) => {
+
+	const [search, setSearch] = useState<string>('');
+	const router = useRouter();
+
+	const goToSearch = (): void => {
+		router.push(`/search?q=${search}`);
+	};
+
+	return (
+		<div className={styles.searchContainer}>
+			<form className={styles.searchForm}>
+				<div className={styles.searchInputWrapper}>
+					<input
+						className={styles.searchInput}
+						placeholder="Що шукаєте?"
+						value={search}
+						onChange={e => setSearch(e.target.value)}
+					/>
+					<div className={styles.searchInputIconWrapper}>
+						<CommonIcon
+							id="icon-search"
+							width="28"
+							height="28"
+							className={styles.searchInputIcon}
+						/>
+					</div>
+				</div>
+				<CommonButton
+					type="submit"
+					title=""
+					color="white"
+					className={styles.searchButton}
+					onClick={goToSearch}
+				>
+					<CommonIcon
+						id="icon-search"
+						width="25"
+						height="25"
+						className={styles.searchButtonIcon}
+					/>
+				</CommonButton>
+			</form>
+		</div>
+	);
 };
 
 export default Search;
