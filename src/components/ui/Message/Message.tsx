85ee32bdd4df3f6657c79fa9be3d706ea92d9a62
a-nester <a_nester@ukr.net>
2024-12-08 19:16:18 +0200

    add/toaster-message-component

---
+++ src/components/ui/Message/Message.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "react-hot-toast";
+
+export const Message = () => {
+  return (
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      gutter={8}
+      containerClassName=""
+      containerStyle={{}}
+      toastOptions={{
+        // Define default options
+        className: "",
+        duration: 5000,
+        style: {
+          maxWidth: "600px",
+          background: "#363636",
+          color: "#fff",
+        },
+
+        // Default options for specific types
+        success: {
+          duration: 3000,
+          theme: {
+            primary: "green",
+            secondary: "black",
+          },
+        },
+      }}
+    />
+  );
+};
+
+export default Message;
