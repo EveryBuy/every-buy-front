7da42f6a6d7a70a22483af1a785babd1282ec14e
AzVika <26838569+AzVika@users.noreply.github.com>
2025-04-14 23:07:37 +0300

    add function favorites in Announcement

--- src/components/pages/announcement/Announcement.tsx
+++ src/components/pages/announcement/Announcement.tsx
@@ -1,153 +1,158 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { getActiveAdvertisement } from "@/redux/advertisement/operations";
 import { RootState, useAppSelector, useAppDispatch } from "@/redux/store";
 import { Advertisement } from "@/redux/advertisement/slice";
 import {
-  Contacts,
-  Info,
-  Seller,
-  AnnouncementSlider,
-  CommonPreloader,
-  CustomSeparator,
+	Contacts,
+	Info,
+	Seller,
+	AnnouncementSlider,
+	CommonPreloader,
+	CustomSeparator,
 } from "@/components";
 import styles from "./Announcement.module.scss";
 import { setHeaderAuthToken } from "@/utils/axios";
 
 export default function Announcement() {
-  const dispatch = useAppDispatch();
-  const searchParams = useSearchParams();
-  const preId: number = Number(searchParams.get("id")) || 0;
-  const id: number = isNaN(preId) ? 0 : preId;
+	const dispatch = useAppDispatch();
+	const searchParams = useSearchParams();
+	const preId: number = Number(searchParams.get("id")) || 0;
+	const id: number = isNaN(preId) ? 0 : preId;
+	const router = useRouter();
 
-  const [isFetching, setIsFetching] = useState(true);
+	const [isFetching, setIsFetching] = useState(true);
 
-  const advertisementById: Advertisement | null = useAppSelector(
-    (state: RootState) => state.advertisement.activeAdvertisement
-  );
-  const advertisementId = advertisementById?.id;
-  const loading = useAppSelector(
-    (state: RootState) => state.advertisement.isLoading
-  );
+	const advertisementById: Advertisement | null = useAppSelector(
+		(state: RootState) => state.advertisement.activeAdvertisement
+	);
+	const advertisementId: number = advertisementById?.id || 0;
+	const loading = useAppSelector(
+		(state: RootState) => state.advertisement.isLoading
+	);
 
-  const fetchData = async () => {
-    setIsFetching(true);
-    const persistData = localStorage.getItem("persist:root");
-    if (persistData) {
-      const parsedPersistData = JSON.parse(persistData);
-      const authToken = JSON.parse(parsedPersistData.token);
+	const fetchData = async () => {
+		setIsFetching(true);
+		// const persistData = localStorage.getItem("persist:root");
+		// if (persistData) {
+		// 	const parsedPersistData = JSON.parse(persistData);
+		// 	const authToken = JSON.parse(parsedPersistData.token);
 
-      if (authToken) {
-        setHeaderAuthToken(authToken);
-      }
-    } else {
-      console.error("No persist data found in localStorage");
-    }
+		// 	if (authToken) {
+		// 		setHeaderAuthToken(authToken);
+		// 	}
+		// } else {
+		// 	console.error("No persist data found in localStorage");
+		// }
 
-    try {
-      await dispatch(getActiveAdvertisement(id)).unwrap();
-    } catch (error) {
-      console.error("Error during fetching:", error);
-    } finally {
-      setIsFetching(false);
-    }
-  };
+		try {
+			await dispatch(getActiveAdvertisement(id)).unwrap();
+		} catch (error) {
+			console.error("Error during fetching:", error);
+		} finally {
+			setIsFetching(false);
+		}
+	};
 
-  useEffect(() => {
-    if (!id) return;
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, id]);
+	useEffect(() => {
+		if (!id || id === 0) {
+			router.push('/');
+		} else {
+			fetchData();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [id]);
 
-  if (isFetching || loading) {
-    return (
-      <div className={styles.preloaderContainer}>
-        <CommonPreloader sx={{ color: "#e5ff46" }} />
-      </div>
-    );
-  }
+	if (isFetching || loading) {
+		return (
+			<div className={styles.preloaderContainer}>
+				<CommonPreloader sx={{ color: "#e5ff46" }} />
+			</div>
+		);
+	}
 
-  if (!advertisementById) {
-    return (
-      <div className={styles.container}>
-        Оголошення було видалено, або його не існувало.
-      </div>
-    );
-  } else {
-    const creationDate = new Date(advertisementById.creationDate);
-    const formattedDate = new Intl.DateTimeFormat("uk-UA", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: false,
-    }).format(creationDate);
 
-    return (
-      <>
-        <div className={styles.breadcrumbsContainer}>
-          <CustomSeparator
-            category={{
-              id: advertisementById.category.id,
-              title: advertisementById.category.nameUkr,
-              link: `/catalogy?categoryId=${advertisementById.category.id}`,
-            }}
-            topSubCategory={
-              advertisementById.topSubCategory
-                ? {
-                    id: advertisementById.topSubCategory.id,
-                    title: advertisementById.topSubCategory.subCategoryNameUkr,
-                    link: `/catalogy?categoryId=${advertisementById.category.id}&topSubCategoryId=${advertisementById.topSubCategory.id}`,
-                  }
-                : null
-            }
-            lowSubCategory={
-              advertisementById.lowSubCategory
-                ? {
-                    id: advertisementById.lowSubCategory.id,
-                    title: advertisementById.lowSubCategory.subCategoryNameUkr,
-                    link: `/catalogy?categoryId=${advertisementById.category.id}&topSubCategoryId=${advertisementById.topSubCategory.id}&lowSubCategoryId=${advertisementById.lowSubCategory.id}`,
-                  }
-                : null
-            }
-          />
-        </div>
+	if (!advertisementById || advertisementId === null) {
+		return (
+			<div className={styles.container}>
+				Оголошення було видалено, або його не існувало.
+			</div>
+		);
+	} else {
+		const creationDate = new Date(advertisementById.creationDate);
+		const formattedDate = new Intl.DateTimeFormat("uk-UA", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: false,
+		}).format(creationDate);
 
-        <div className={styles.container}>
-          <AnnouncementSlider images={advertisementById?.photoUrls || []} />
-          <Contacts
-            contactsInfo={{
-              publicDate: formattedDate,
-              cost: advertisementById.price,
-              delivery: advertisementById.deliveryMethods,
-              title: advertisementById.title,
-              section: advertisementById.section,
-            }}
-            advertisementId={advertisementId}
-          />
-          <Info
-            articleInfo={{
-              description: advertisementById.description,
-              location: {
-                city: advertisementById.city.cityName,
-                region: advertisementById.city.region.regionName,
-              },
-              delivery: advertisementById.deliveryMethods,
-            }}
-          />
-          <Seller
-            sellerInfo={{
-              imageUrl: advertisementById.userDto.photoUrl,
-              nameUkr: advertisementById.userDto.fullName,
-              online: advertisementById.isEnabled,
-              linkToAllAdvert: `/seller/${advertisementById.userId}`,
-            }}
-          />
-        </div>
-      </>
-    );
-  }
+		return (
+			<>
+				<div className={styles.breadcrumbsContainer}>
+					<CustomSeparator
+						category={{
+							id: advertisementById.category.id,
+							title: advertisementById.category.nameUkr,
+							link: `/catalogy?categoryId=${advertisementById.category.id}`,
+						}}
+						topSubCategory={
+							advertisementById.topSubCategory
+								? {
+									id: advertisementById.topSubCategory.id,
+									title: advertisementById.topSubCategory.subCategoryNameUkr,
+									link: `/catalogy?categoryId=${advertisementById.category.id}&topSubCategoryId=${advertisementById.topSubCategory.id}`,
+								}
+								: null
+						}
+						lowSubCategory={
+							advertisementById.lowSubCategory
+								? {
+									id: advertisementById.lowSubCategory.id,
+									title: advertisementById.lowSubCategory.subCategoryNameUkr,
+									link: `/catalogy?categoryId=${advertisementById.category.id}&topSubCategoryId=${advertisementById.topSubCategory.id}&lowSubCategoryId=${advertisementById.lowSubCategory.id}`,
+								}
+								: null
+						}
+					/>
+				</div>
+
+				<div className={styles.container}>
+					<AnnouncementSlider images={advertisementById?.photoUrls || []} />
+					{/* <Contacts
+						contactsInfo={{
+							publicDate: formattedDate,
+							cost: advertisementById.price,
+							delivery: advertisementById.deliveryMethods,
+							title: advertisementById.title,
+							section: advertisementById.section,
+						}}
+						advertisementId={advertisementId}
+					/> */}
+					<Info
+						articleInfo={{
+							description: advertisementById.description,
+							location: {
+								city: advertisementById.city.cityName,
+								region: advertisementById.city.region.regionName,
+							},
+							delivery: advertisementById.deliveryMethods,
+						}}
+					/>
+					<Seller
+						sellerInfo={{
+							imageUrl: advertisementById.userDto.photoUrl,
+							nameUkr: advertisementById.userDto.fullName,
+							online: advertisementById.isEnabled,
+							linkToAllAdvert: `/seller/${advertisementById.userId}`,
+						}}
+					/>
+				</div>
+			</>
+		);
+	}
 }
--- src/components/pages/announcement/Contacts/Contacts.tsx
+++ src/components/pages/announcement/Contacts/Contacts.tsx
@@ -2,110 +2,187 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { CommonIcon, CommonButton } from "@/components";
+import { CommonIcon, CommonButton, DoLoginModal } from "@/components";
 import styles from "./Contacts.module.scss";
 import { deliveryToString } from "../deliveryToString";
 import { useCreateChatMutation } from "@/redux/messages/chatApi";
-import { useDispatch } from "react-redux";
+import { useAppSelector, useAppDispatch } from "@/redux/store";
 import { setNewChatId } from "@/redux/messages/slice";
+import { FavouriteAdvertisement } from '@/redux/advertisement/slice';
+import { getAllFavouriteAdvert, addAdvertToFavourite, removeAdvertFromFavourite } from '@/redux/advertisement/operations';
+import { selectIsLoggedIn } from "@/redux/auth/selectors";
+
 interface ContactsProps {
-  contactsInfo: {
-    publicDate: string;
-    cost: number;
-    delivery: string[];
-    title: string;
-    phoneNumber?: string;
-    section: string;
-  };
-  advertisementId: number | undefined;
+	contactsInfo: {
+		publicDate: string;
+		cost: number;
+		delivery: string[];
+		title: string;
+		phoneNumber?: string;
+		section: string;
+	};
+	advertisementId: number;
 }
 
 export default function Contacts({
-  contactsInfo,
-  advertisementId,
+	contactsInfo,
+	advertisementId,
 }: ContactsProps) {
-  const { publicDate, cost, delivery, title, phoneNumber, section } =
-    contactsInfo;
+	const { publicDate, cost, delivery, title, phoneNumber, section } =
+		contactsInfo;
 
-  const [showNumber, setShowNumber] = useState(false);
-  const [createChat] = useCreateChatMutation();
-  const [chatId, setChatId] = useState<number | null>(null);
-  const router = useRouter();
-  const dispatch = useDispatch();
+	const [showNumber, setShowNumber] = useState(false);
+	const [createChat] = useCreateChatMutation();
+	const [chatId, setChatId] = useState<number | null>(null);
+	const [heartSelect, setHeartSelect] = useState<boolean>(false);
 
-  const sectionLabel =
-    section === "SELL" ? "Продаж" : section === "BUY" ? "Купівля" : "Невідомо";
+	const router = useRouter();
+	const dispatch = useAppDispatch();
 
-  const getNewChatIdHandler = async () => {
-    try {
-      if (advertisementId) {
-        const response = await createChat({ advId: advertisementId }).unwrap();
-        console.log(response);
+	const sectionLabel =
+		section === "SELL" ? "Продаж" : section === "BUY" ? "Купівля" : "Невідомо";
 
-        const newChatId = response?.id;
-        if (newChatId) {
-          setChatId(newChatId);
-          dispatch(setNewChatId(newChatId));
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    if (chatId) {
-      router.push(`/messages`);
-    }
-  }, [chatId, router]);
 
-  return (
-    <div className={styles.list}>
-      <div className={styles.titleItem}>
-        <CommonIcon
-          id="heart"
-          width="30"
-          height="28"
-          className={styles.favoriteSvg}
-        />
-        <div className={styles.publicBox}>
-          <p className={styles.public}>Опубліковано {publicDate}</p>
-          <p className={styles.public}>{sectionLabel}</p>
-        </div>
-        <h1 className={styles.title}>{title}</h1>
-        <div>
-          <p className={styles.changeWidth}>Вартість</p>
-          <p className={styles.price}>{cost} грн</p>
-        </div>
-        <div className={styles.descVersion}>
-          <div className={styles.deliveryBox}>
-            <p className={styles.deliveryTitle}>Спосіб доставки</p>
-            <p className={styles.text}>{deliveryToString(delivery)}</p>
-          </div>
-          <div className={styles.buttonsItem}>
-            <CommonButton
-              type="button"
-              title=""
-              color="transparent"
-              className={styles.yellowBorderButton}
-              onClick={() => getNewChatIdHandler()}
-            >
-              Надіслати повідомлення
-            </CommonButton>
-            {!showNumber && (
-              <CommonButton
-                type="button"
-                color="transparent"
-                title=""
-                className={styles.yellowBorderButton}
-                onClick={() => setShowNumber(true)}
-              >
-                Показати телефон
-              </CommonButton>
-            )}
-            {showNumber && <p className={styles.phoneNumber}>0976709876</p>}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const [successRegisterModalOpen, setSuccessRegisterModalOpen] = useState(false);
+	const isLoggedIn = useAppSelector(selectIsLoggedIn);
+	const openWindowHandle = () => {
+		!isLoggedIn ? setSuccessRegisterModalOpen((prev) => !prev) : null;
+	};
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			dispatch(getAllFavouriteAdvert({ section }));
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [section]);
+
+	const getNewChatIdHandler = async () => {
+		if (!isLoggedIn) {
+			openWindowHandle();
+		} else {
+			try {
+				if (advertisementId) {
+					const response = await createChat({ advId: advertisementId }).unwrap();
+					console.log(response);
+
+					const newChatId = response?.id;
+					if (newChatId) {
+						setChatId(newChatId);
+						dispatch(setNewChatId(newChatId));
+					}
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
+	}
+
+	useEffect(() => {
+		if (chatId) {
+			router.push(`/messages`);
+		}
+	}, [chatId, router]);
+
+	const favourites: FavouriteAdvertisement[] = useAppSelector(state => state.advertisement.favouriteAdvertisements);
+
+	useEffect(() => {
+
+		if (isLoggedIn) {
+			const favArr: FavouriteAdvertisement[] = favourites.filter(item => item.advertisementId === advertisementId);
+			(favArr.length > 0) ? setHeartSelect(true) : setHeartSelect(false);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [favourites]);
+
+	const showPhoneNumber = (): void => {
+		if (!isLoggedIn) {
+			openWindowHandle()
+		} else {
+			setShowNumber(true);
+		}
+	}
+
+	const fetchDataHeart = async (id: number, isFamouse: boolean) => {
+		try {
+			if (isFamouse) {
+				await dispatch(removeAdvertFromFavourite(id)).unwrap();
+			} else {
+				await dispatch(addAdvertToFavourite(id)).unwrap();
+			}
+			setHeartSelect(prev => !prev);
+		} catch (error) {
+			console.error("Error during fetching:", error);
+		}
+	};
+
+	const addSelectGood = (id: number): void => {
+		// e.preventDefault();
+		isLoggedIn ? fetchDataHeart(id, heartSelect) : openWindowHandle();
+	}
+
+
+	return (
+		<div className={styles.list}>
+			<div className={styles.titleItem}>
+				<CommonButton
+					type="submit"
+					title=""
+					className={styles.favoriteBtn}
+					onClick={() => addSelectGood(advertisementId)}
+				>
+					<CommonIcon
+						id={heartSelect ? "heart" : "icon-heart"}
+						width="36"
+						height="36"
+						className={styles.favoriteSvg}
+					/>
+				</CommonButton>
+				<div className={styles.publicBox}>
+					<p className={styles.public}>Опубліковано {publicDate}</p>
+					<p className={styles.public}>{sectionLabel}</p>
+				</div>
+				<h1 className={styles.title}>{title}</h1>
+				<div>
+					<p className={styles.changeWidth}>Вартість</p>
+					<p className={styles.price}>{cost} грн</p>
+				</div>
+				<div className={styles.descVersion}>
+					<div className={styles.deliveryBox}>
+						<p className={styles.deliveryTitle}>Спосіб доставки</p>
+						<p className={styles.text}>{deliveryToString(delivery)}</p>
+					</div>
+					<div className={styles.buttonsItem}>
+						<CommonButton
+							type="button"
+							title=""
+							color="transparent"
+							className={styles.yellowBorderButton}
+							onClick={() => getNewChatIdHandler()}
+						>
+							Надіслати повідомлення
+						</CommonButton>
+						{!showNumber && (
+							<CommonButton
+								type="button"
+								color="transparent"
+								title=""
+								className={styles.yellowBorderButton}
+								onClick={() => showPhoneNumber()}
+							>
+								Показати телефон
+							</CommonButton>
+						)}
+						{showNumber && <p className={styles.phoneNumber}>0976709876</p>}
+
+						{successRegisterModalOpen && (
+							<DoLoginModal
+								doModalOpen={setSuccessRegisterModalOpen}
+								openWindowHandle={openWindowHandle}
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
