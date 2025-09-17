3a1ce506eab3ddb283f366bb80591e6b07445b43
AzVika <26838569+AzVika@users.noreply.github.com>
2025-09-17 20:13:47 +0300

    add metadata for MainPage

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,18 +1,34 @@
-"use client";
-
-import { Search, SliderContainer, Category } from "@/components";
-import { Suspense } from "react";
+import MainPage from './mainPage';
+import { Metadata } from "next";
 // import ProtectedRoute from "../../components/auth/Login/ProtectedRoute/ProtectedRoute";
+
+export const metadata: Metadata = {
+	title: "Головна сторінка EveryBuy - сучасна платформа для створення оголошень",
+	description: "EveryBuy - сучасна платформа для створення оголошень для продажу та купівля товарів або послуг",
+	keywords: ["EveryBuy", "сучасна", "платформа", "оголошення", "продажу", "купівлі", "товарів", "послуг"],
+	openGraph: {
+		title: "EveryBuy - сучасна платформа для створення оголошень",
+		description: "EveryBuy - сучасна платформа для створення оголошень для продажу та купівля товарів або послуг",
+		url: "https://everybuy.pp.ua",
+		siteName: "EveryBuy",
+		// images: [
+		// 	{
+		// 		url: "https://everybuy.pp.ua/",
+		// 		width: 1200,
+		// 		height: 630,
+		// 		alt: "Превʼю сайту everybuy",
+		// 	},
+		// ],
+		locale: "uk_UA",
+		type: "website",
+	},
+};
 
 export default function HomePage() {
 	return (
 		<>
 			{/* <ProtectedRoute> */}
-			<Suspense fallback={<div>Loading...</div>}>
-				<Search style={{ margin: "0 20px" }} />
-			</Suspense>
-			<SliderContainer />
-			<Category />
+			< MainPage />
 			{/* </ProtectedRoute> */}
 		</>
 	);
